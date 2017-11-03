d8fc0528e42b73fdae3daee76da51bd938655c23
hiddenontown <86153298@qq.com>
2017-11-03 10:28:24 +0800

    change drag.js

--- drag.js
+++ drag.js
@@ -10,10 +10,15 @@ $(function(){
 	})
 
 	function drag(id) {
+
 		var obj = document.getElementById(id);
+		var disX = 0;
+		var disY = 0;
+
 		obj.onmousedown = function(){
-			var disX = 0;
-			var disY = 0;
+
+			disX = ev.pageX - obj.offsetLeft;
+			disY = ev.pageY - obj.offsetTop;
 		}
 	}
 
